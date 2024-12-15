17cf1745017a7064c145c482da1397958c28a583
owusuvictorjnr <owusuvictor80@gmail.com>
2024-12-15 15:04:28 +0000

    remove parser and unified topology

--- db.js
+++ db.js
@@ -1,16 +1,14 @@
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB");
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("DB connected successfully...");
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    console.error("Database connection error:", error);
     process.exit(1);
   }
 };
